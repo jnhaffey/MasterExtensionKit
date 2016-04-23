53d7645aa131cf155b326fc720e73a245d290327
Jason N Haffey <jnhaffey@gmail.com>
2016-04-23 18:33:08 +0100

    Added new DateTime Extension

--- MasterExtensionKit.Datetimes.UnitTests/Functions/DateDifferenceExtensionsUnitTests.cs
+++ MasterExtensionKit.Datetimes.UnitTests/Functions/DateDifferenceExtensionsUnitTests.cs
@@ -39,5 +39,24 @@ namespace MasterExtensionKit.Datetimes.UnitTests.Functions
 		}
 
 		#endregion
+
+		#region StartOfWeek Tests
+
+		[TestMethod]
+		public void StartOfWeek_2000_1_1_Monday()
+		{
+			var dateToTest = new DateTime(2000,1,1);
+			var expectedResult = new DateTime(1999,12,27);
+			Assert.AreEqual(expectedResult, dateToTest.StartOfWeek(DayOfWeek.Monday));
+		}
+
+		[TestMethod]
+		public void StartOfWeek_2001_1_1_Sunday()
+		{
+			var dateToTest = new DateTime(2001,1,1);
+			var expectedResult = new DateTime(2000,12,31);
+			Assert.AreEqual(expectedResult, dateToTest.StartOfWeek(DayOfWeek.Sunday));
+		}
+		#endregion
 	}
 }
--- MasterExtensionKit.Datetimes/Functions/DateDifferenceExtensions.cs
+++ MasterExtensionKit.Datetimes/Functions/DateDifferenceExtensions.cs
@@ -18,5 +18,22 @@ namespace MasterExtensionKit.Datetimes.Functions
 			}
 			return today.Year - date.Year;
 		}
+
+		/// <summary>
+		///     Get the start of the week for the given date
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="startOfWeek"></param>
+		/// <returns>DateTime</returns>
+		public static DateTime StartOfWeek(this DateTime value, DayOfWeek startOfWeek = DayOfWeek.Monday)
+		{
+			var diff = value.DayOfWeek - startOfWeek;
+			if (diff < 0)
+			{
+				diff += 7;
+			}
+
+			return value.AddDays(-1*diff).Date;
+		}
 	}
 }
