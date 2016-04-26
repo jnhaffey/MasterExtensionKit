2474eff9f06c1df5b12769fa163d148454bc4f7c
Jason N Haffey <jnhaffey@gmail.com>
2016-04-26 12:28:31 +0100

    Delete CommonAssemblyInfo.cs
    Restructuring for Version 2.0

--- CommonAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("Jason N Haffey")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyProduct("Master Extension Kit")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
